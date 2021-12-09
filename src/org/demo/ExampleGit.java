93f72ccdc8ad157c69383c708994249e671940f7
ArunKumar9798 <arunhere9798@gmail.com>
2021-12-09 18:19:48 +0530

    task completed

---
+++ src/org/demo/ExampleGit.java
@@ -0,0 +1,16 @@
+package org.demo;
+
+public class ExampleGit {
+
+	private void add() {
+    System.out.println("add");
+	}
+	
+	public static void main(String[] args) {
+		ExampleGit e=new ExampleGit();
+	    e.add();
+	}
+
+	
+			
+}
